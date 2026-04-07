7eec4fc54918150adf929ee1df4b5522cca71d5b
Nguyen Minh Tri <nguyenminhtri18072005@gmail.com>
2026-04-07 23:15:32 +0200

    Add cross-platform (Windows/Linux) socket typedefs and macros in app_context.h

---
+++ network/includes/app_context.h
@@ -0,0 +1,39 @@
+#ifndef APP_CONTEXT_H
+#define APP_CONTEXT_H
+
+//Windows
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+typedef SOCKET socket_t;
+typedef int socklen_t;
+#define INVALID_FD INVALID_SOCKET
+#else
+// Linux/Unix
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+typedef int socket_t;
+#define INVALID_FD -1
+#endif
+
+#define MAX_PEERS 100
+#define BUF_SIZE 1024
+
+typedef struct {
+    socket_t fd;
+    struct sockaddr_in addr;
+    int active;
+} Peer;
+
+typedef struct {
+    socket_t listen_fd;
+    socket_t python_fd;
+    Peer peers[MAX_PEERS];
+    int running;
+} AppContext;
+
+void init_app_context(AppContext *ctx);
+
+#endif
