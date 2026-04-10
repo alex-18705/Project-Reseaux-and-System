b689517ca61d1280ebfccd84d2ac500dbc767ee8
Neantdinf0 <lucie.ratie@insa-cvl.fr>
2026-04-10 10:10:52 +0200

    Conversion de map vers json

--- test_json.py
+++ test_json.py
@@ -1,11 +1,14 @@
-import argparse
-import os
 import json
 
 from backend.Class.Army import Army
 from backend.Utils.file_loader import load_mirrored_army_from_file, load_map_from_file
 from backend.Class.Units.Crossbowman import Crossbowman
 from backend.Class.Units.Knight import Knight
+from backend.Class.Map import Map
+from backend.Class.Obstacles.Obstacle import Obstacle
+
+from backend.Class.Units.Unit import Unit
+
 
 def main():
 
@@ -14,20 +17,31 @@ def main():
 
         print(army1.units, "\n")
         army_to_json(army1)
+        print(map_obj.obstacles, "\n")
+        map_to_json(map_obj)
 
-UNIT_CLASSES = {
-    "Crossbowman": Crossbowman,
-    "Knight": Knight,
-}
 
 def unit_to_dict(unit):
     return {
-        "type": unit.unit_type(),
+        "type": unit.__class__.__name__,
         "hp": unit.hp,
         "position": unit.position,
         "cooldown": unit.cooldown,
         "last_attacker": unit.last_attacker,
         "last_attacked": unit.last_attacked,
+    }
+
+def obstacle_to_dict(obstacle):
+    return {
+        "size": obstacle.size,
+        "position": obstacle.position,
+    }
+
+def map_to_dict(map):
+    return {
+        "width": map.width,
+        "height": map.height,
+        "obstacles": (obstacle_to_dict(o) for o in map.obstacles),
     }
 
 def army_to_json(army):
@@ -41,7 +55,7 @@ def json_to_army(filepath="army.json"):
         units_data = json.load(json_file)
     army = Army()
     for d in units_data:
-        cls = UNIT_CLASSES[d["type"]]
+        cls = d["type"]
         if cls not in UNIT_CLASSES:
             continue
         unit = cls(
@@ -53,12 +67,13 @@ def json_to_army(filepath="army.json"):
         unit.last_attacked = d["last_attacked"]
 
 
-def map_obj_to_json(map_obj):
+def map_to_json(map_obj):
     print(map_obj)
     pass
-def json_to_map_obj(json):
+def json_to_map(json):
     pass
 
-"""print(army_to_json(army1))"""
+#Appel de main pour contrôler
 main()
+
 
